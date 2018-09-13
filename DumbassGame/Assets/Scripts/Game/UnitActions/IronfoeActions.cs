1ad064ec4caf9aa0684d1d8483cf7410e1766e7a
JoeshuaBaker <joeshuabaker@gmail.com>
2018-09-13 13:18:44 -0700

    added fuckin big boy
    tried to make it work, cried

---
+++ DumbassGame/Assets/Scripts/Game/UnitActions/IronfoeActions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronfoeActions : ActionController {
+	
+	override public void Attack(Vector3 attackPos, Vector3 targetDirection)
+	{
+
+	}
+
+	override public bool CancelAttack()
+	{
+		return false;
+	}
+}
---
+++ DumbassGame/Assets/Scripts/Game/UnitAnimations/IronfoeAnimations.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronfoeAnimations : AnimationController {
+
+	override public void SetIdle()
+	{
+		anim.SetTrigger("Stop");
+	}
+	override public void SetMove(bool moving)
+	{
+		anim.SetBool("Moving", moving);
+	}
+}
